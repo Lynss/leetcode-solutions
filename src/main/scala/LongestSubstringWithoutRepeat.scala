601e7ac3b31583bf096f36685ecc504b8e4933fd
龙云 <longyun@co-mall.com>
2018-11-23 18:17:49 +0800

    +1

--- src/main/scala/LongestSubstringWithoutRepeat.scala
+++ src/main/scala/LongestSubstringWithoutRepeat.scala
@@ -1,5 +1,15 @@
 object LongestSubstringWithoutRepeat extends App {
-//  def lengthOfLongestSubstring(s: String): Int = {
-//  }
-//  println(lengthOfLongestSubstring("dvdf"))
+  def lengthOfLongestSubstring(s: String): Int = {
+    val a = s.toList
+
+    def findMaxLength(m: Int, p: List[Char], l: List[Char]): Int = l match {
+      case Nil => m
+      case x :: xs =>
+        val newPrefix = p.drop(p.indexOf(x) + 1) :+ x
+        findMaxLength(m max newPrefix.length, newPrefix, xs)
+    }
+
+    findMaxLength(0, Nil, a)
+  }
+  println(lengthOfLongestSubstring("dvdf"))
 }
